6ae196c0cef5cdb079a5b52cf9891f639024cc42
kk69njit <kk69@njit.edu>
2024-02-01 20:24:30 -0500

    added names.java

---
+++ names.java
@@ -0,0 +1,9 @@
+class names{
+    public static void main(String []args){
+        System.out.println("Karan Komal");
+        System.out.println("Frank Xu");
+        System.out.println("Jose Ventura");
+        System.out.println("Moiez Q.");
+        System.out.println("Andrew R.");
+    }
+};
